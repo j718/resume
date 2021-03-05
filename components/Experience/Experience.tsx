bed77f7ca4c568bec5eda7253196d99c6d6441e5
Jacob Clarke <jacobclarke718@gmail.com>
2021-03-05 13:56:24 +0000

    added viewport animation

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -1,4 +1,7 @@
 import { Typography, Grid } from "@material-ui/core";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export interface ExperienceProps {
   title: string;
@@ -8,26 +11,52 @@ export interface ExperienceProps {
   description?: string;
 }
 
-export const Experience = (props: ExperienceProps) => (
-  <div>
-    <Typography variant="h5">{props.title}</Typography>
-    <Grid container justify="space-between">
-      <Grid item>
-        <Typography variant="subtitle1">{props.subtitle}</Typography>
+export const Experience = (props: ExperienceProps) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  return (
+    <div>
+      <Typography
+        ref={ref}
+        variant="h5"
+        component={motion.div}
+        initial="visible"
+        animate={controls}
+        transition={{ duration: 0.3 }}
+        variants={{
+          visible: { x: 0 },
+          hidden: { x: -20 },
+        }}
+      >
+        {props.title}
+      </Typography>
+      <Grid container justify="space-between">
+        <Grid item>
+          <Typography variant="subtitle1">{props.subtitle}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body2">{props.timeline}</Typography>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="body2">{props.timeline}</Typography>
-      </Grid>
-    </Grid>
-    <Typography variant="body1">{props.description}</Typography>
-    <ul>
-      {props.achievements?.map((item, index) => (
-        <li key={index}>
-          <Typography variant="body1">{item}</Typography>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+      <Typography variant="body1">{props.description}</Typography>
+      <ul>
+        {props.achievements?.map((item, index) => (
+          <li key={index}>
+            <Typography variant="body1">{item}</Typography>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Experience;
