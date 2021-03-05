92f2edb73ae6dab05c146b1e65d35fb30f2c008f
Jacob Clarke <jacobclarke718@gmail.com>
2021-03-05 13:26:31 +0000

    added framer

--- containers/Intro/Intro.tsx
+++ containers/Intro/Intro.tsx
@@ -1,5 +1,6 @@
 import { makeStyles, Typography, Grid, Avatar } from "@material-ui/core";
 import { name, intro } from "../../data";
+import { motion } from "framer-motion";
 
 const useStyles = makeStyles((theme) => ({
   avatar: {
@@ -7,20 +8,36 @@ const useStyles = makeStyles((theme) => ({
     height: "auto",
     margin: "auto",
     marginBottom: theme.spacing(2),
+    minHeight: theme.spacing(20),
     marginTop: theme.spacing(2),
     maxHeight: theme.spacing(30),
     maxWidth: theme.spacing(30),
+  },
+  root: {
+    minHeight: theme.spacing(20),
   },
 }));
 
 export default function Intro() {
   const classes = useStyles();
   return (
-    <Grid container spacing={4} alignItems="center" justify="space-around">
+    <Grid
+      className={classes.root}
+      container
+      spacing={4}
+      alignItems="center"
+      justify="space-around"
+    >
       <Grid item xs={12} md={3}>
         <Avatar className={classes.avatar} src="/headshot.jpg" />
       </Grid>
-      <Grid item md={9}>
+      <Grid
+        component={motion.div}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        item
+        md={9}
+      >
         <Typography variant="h2">{name}</Typography>
         <Typography variant="body1">{intro}</Typography>
       </Grid>
