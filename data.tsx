5b466d8c71517f167870b01a58616e055d19793a
Jacob Clarke <jacobclarke718@gmail.com>
2021-03-05 02:25:38 +0000

    added step

--- data.tsx
+++ data.tsx
@@ -112,6 +112,7 @@ export const education: Array<EducationProps> = [
     institution: "St. Louis University School of Medicine",
     degree: "MD Candidate",
     timeline: "Aug 2018- Present",
+    achievements: ["USMLE Step 1: 243"],
   },
   {
     institution: "Brigham Young University",
